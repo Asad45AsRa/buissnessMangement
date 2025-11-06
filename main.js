7eb67f919c29be955bc108ea0eedf680637b7474
unknown <king234asad@gmail.com>
2025-11-06 14:58:16 +0500

    Al Fatah Spray

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: path.join(__dirname, 'assets/icon.png')
+  });
+
+  mainWindow.loadFile('renderer/index.html');
+  mainWindow.maximize();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
